ffc92bfb7bdbf318db1278222ea185216824ba57
grapebanana <128008463+grapebanana@users.noreply.github.com>
2024-03-21 14:48:48 +0900

    ㅇㄹㅇ

---
+++ Asihana/src/com/kh/semi/community/controller/communityController.java
@@ -0,0 +1,44 @@
+package com.kh.semi.community.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class communityController
+ */
+@WebServlet("/readcommu")
+public class communityController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public communityController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	
+	
+	
+	
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ Asihana/src/com/kh/semi/community/model/service/communityService.java
@@ -0,0 +1,5 @@
+package com.kh.semi.community.model.service;
+
+public class communityService {
+
+}

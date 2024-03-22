1868ea8c6ca585481c38b4f4b376d54e5a0a470b
LeeSeongWook1 <tjddnr0761@gmail.com>
2024-03-22 15:31:21 +0900

    로그아웃 세션

--- Asihana/src/com/kh/semi/member/controller/LogoutController.java
+++ Asihana/src/com/kh/semi/member/controller/LogoutController.java
@@ -1,5 +1,48 @@
 package com.kh.semi.member.controller;
 
-public class LogoutController {
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LogoutController
+ */
+@WebServlet("/logout")
+public class LogoutController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LogoutController() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		HttpSession session = request.getSession();
+		session.invalidate();
+		
+		RequestDispatcher view = request.getRequestDispatcher("index.jsp");
+		view.forward(request,response);
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
 
 }

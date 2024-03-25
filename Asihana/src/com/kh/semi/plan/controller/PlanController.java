3ef08109e83a069160e43d6b851a0a298d682ebf
WonjunJung <jun1221909@gmail.com>
2024-03-25 15:35:39 +0900

    플랜 상세 조회

--- Asihana/src/com/kh/semi/plan/controller/PlanController.java
+++ Asihana/src/com/kh/semi/plan/controller/PlanController.java
@@ -26,6 +26,14 @@ public class PlanController {
 		
 		return view;
 	}
+
+	public String selectPlan(HttpServletRequest request, HttpServletResponse response) {
+		
+		
+		String view = "views/plan/planDetail.jsp";
+		
+		return view;
+	}
 	
 	
 

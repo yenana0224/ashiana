71b6a83895fcbeb0201feda43081b4bc76a3f6b1
WonjunJung <jun1221909@gmail.com>
2024-03-25 11:41:02 +0900

    servlet

--- Asihana/src/com/kh/semi/plan/controller/PlanServletController.java
+++ Asihana/src/com/kh/semi/plan/controller/PlanServletController.java
@@ -11,13 +11,13 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class PlanController
  */
 @WebServlet("*.plan")
-public class PlanController extends HttpServlet {
+public class PlanServletController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public PlanController() {
+    public PlanServletController() {
         super();
         // TODO Auto-generated constructor stub
     }

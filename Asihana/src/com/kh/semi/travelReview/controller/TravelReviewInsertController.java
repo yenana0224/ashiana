9b587bb08709fdc1a309ae9843761a8f3360d50b
AllenChoi <162558154+whiteholse@users.noreply.github.com>
2024-03-29 11:52:50 +0900

    a

--- Asihana/src/com/kh/semi/travelReview/controller/TravelReviewInsertController.java
+++ Asihana/src/com/kh/semi/travelReview/controller/TravelReviewInsertController.java
@@ -1,11 +1,14 @@
 package com.kh.semi.travelReview.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
 
 /**
  * Servlet implementation class TravelReviewInsertController
@@ -26,9 +29,28 @@ public class TravelReviewInsertController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
 		
-		request.getRequestDispatcher("views/travelReview/travelReviewInsert.jsp").forward(request, response);
+		request.setCharacterEncoding("UTF-8");
+		
+		if(ServletFileUpload.isMultipartContent(request)) {
+			
+			// 1) MultipartRequest 생성
+			// 1_1) 용량제한(10MB)
+			int MaxSize = 1024 * 1024 * 10;
+			
+			// 1_2) 저장할 경로 구함
+			String savePath = request.getServletContext().getRealPath("/resources/travelReview");
+			
+			String content = request.getParameter("content");
+			
+			System.out.println(content);
+			
+			
+			request.getSession().getAttribute("hashTagList");
+			
+			request.getRequestDispatcher("views/travelReview/travelReviewInsert.jsp").forward(request, response);
+		}
+		
 		
 	}
 

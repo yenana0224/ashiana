546b1799eef014c3f4e1bf0aabbca6c260b7ed51
AllenChoi <162558154+whiteholse@users.noreply.github.com>
2024-04-04 16:33:14 +0900

    345

--- Asihana/src/com/kh/semi/travelReview/controller/TravelReviewUpdateController.java
+++ Asihana/src/com/kh/semi/travelReview/controller/TravelReviewUpdateController.java
@@ -1,18 +1,20 @@
 package com.kh.semi.travelReview.controller;
 
 import java.io.IOException;
-import java.util.List;
+import java.util.Arrays;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-import com.kh.semi.common.AttachmentFile;
-import com.kh.semi.travelReview.model.service.TravelReviewService;
-import com.kh.semi.travelReview.model.vo.HashTag;
-import com.kh.semi.travelReview.model.vo.TravelReview;
+import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
+
+import com.kh.semi.common.MyFileRenamePolicy;
+import com.oreilly.servlet.MultipartRequest;
 
 /**
  * Servlet implementation class TravelReviewUpdateController
@@ -33,7 +35,45 @@ public class TravelReviewUpdateController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		
+		// 인코딩
+		request.setCharacterEncoding("UTF-8");
+		
+		if(ServletFileUpload.isMultipartContent(request)) {
+			
+			// 용량 제한
+			int maxSize = 1024 * 1024 * 10;
+			
+			// 1_2) 저장할 경로 구함
+			HttpSession session = request.getSession();
+			ServletContext application = session.getServletContext();
+			String savePath = application.getRealPath("/resources/travelReview/");
+			
+			// 2) MultipartRequest 객체 생성하면서 파일의 이름을 수정하면서 업로드
+			MultipartRequest multiRequest =
+					new MultipartRequest(request, savePath, maxSize, "UTF-8", new MyFileRenamePolicy());
+			
+			String reviewWriter = multiRequest.getParameter("userNo");
+			String reviewTitle = multiRequest.getParameter("title");
+			String reviewContent = multiRequest.getParameter("content");
+			String arrivalDate = multiRequest.getParameter("arrival");
+			String departureDate = multiRequest.getParameter("departure");
+			int cityNo = Integer.parseInt(multiRequest.getParameter("city"));
+			String partner = multiRequest.getParameter("partner");
+			int starPoint = Integer.parseInt(multiRequest.getParameter("star"));
+			String planCheck = multiRequest.getParameter("planCheck");
+			String[] hashTagList = multiRequest.getParameterValues("hashTag");
+			String status = multiRequest.getParameter("status");
+			
+			System.out.println(reviewWriter + "작성자");
+			System.out.println(reviewTitle + "제목");
+			System.out.println(reviewContent + "내용");
+			System.out.println(arrivalDate + "떠나날");
+			System.out.println(cityNo + "도시");
+			System.out.println(partner + "파트너");
+			System.out.println(departureDate + "복구날");
+			System.out.println(Arrays.toString(hashTagList));
+		}	
 		
 	}
 

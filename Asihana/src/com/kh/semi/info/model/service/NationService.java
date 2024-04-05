16925d4afeb82e440e42d910577c27a26c85f2fe
anyeju <162558210+anyeju@users.noreply.github.com>
2024-04-05 17:12:26 +0900

    ㅇㅇㅇㅇㅇㅇ

--- Asihana/src/com/kh/semi/info/model/service/NationService.java
+++ Asihana/src/com/kh/semi/info/model/service/NationService.java
@@ -53,59 +53,53 @@ public class NationService {
 	public int updateNation(int oldNo, int visaNo, Nation nation, String[] volNo, String[] curNo, String[] langNo, AttachmentFile title, AttachmentFile file) {
 
 		Connection conn = getConnection();
-
-		// 기존 번호에 있는 정보가 있다면 전부 삭제
-		if(new InfoDao().countCurrency(conn, oldNo) > 0) new InfoDao().deleteCurrency(conn, oldNo);
-		if(new InfoDao().countLanguage(conn, oldNo) > 0) new InfoDao().deleteLanguage(conn, oldNo);
-		if(new InfoDao().countVoltage(conn, oldNo) > 0) new InfoDao().deleteVoltage(conn, oldNo);
-		if(new NationDao().selectTitlePhoto(conn, oldNo) != null) new NationDao().deleteTitlePhoto(conn, oldNo);
-		if(new NationDao().selectPhoto(conn, oldNo) != null) new NationDao().deletePhoto(conn, oldNo);
-		new InfoDao().deleteVisa(conn, oldNo);
-		new NationDao().deleteNation(conn, oldNo);
-		
-		int result = 0;
 		int nationNo = nation.getNationNo();
+		
+		// 기존 번호에 있는 정보가 있다면 전부 삭제
+		int result = new NationDao().deleteNation(conn, oldNo);
 
-		int nationResult = new NationDao().insertNation(conn, nation);
+		if(result > 0) {
+			int nationResult = new NationDao().insertNation(conn, nation);
 
-		if(nationResult > 0 ) {
-			
-			int visaResult = new InfoDao().nationVisa(conn, visaNo, nationNo);
-			int volResult = 1;
-			if(volNo != null) {
-				for(int i = 0; i < volNo.length; i++) {
-					int voltageNo = Integer.parseInt(volNo[i]);
-					volResult = volResult * new InfoDao().nationVol(conn, voltageNo, nationNo);
+			if(nationResult > 0 ) {
+				
+				int visaResult = new InfoDao().nationVisa(conn, visaNo, nationNo);
+				int volResult = 1;
+				if(volNo != null) {
+					for(int i = 0; i < volNo.length; i++) {
+						int voltageNo = Integer.parseInt(volNo[i]);
+						volResult = volResult * new InfoDao().nationVol(conn, voltageNo, nationNo);
+					}
 				}
-			}
-			
-			int curResult = 1;
-			if(curNo != null) {
-				for(int i = 0; i < curNo.length; i++) {
-					int currencyNo = Integer.parseInt(curNo[i]);
-					curResult = curResult * new InfoDao().nationCur(conn, currencyNo, nationNo);
+				
+				int curResult = 1;
+				if(curNo != null) {
+					for(int i = 0; i < curNo.length; i++) {
+						int currencyNo = Integer.parseInt(curNo[i]);
+						curResult = curResult * new InfoDao().nationCur(conn, currencyNo, nationNo);
+					}
 				}
-			}
-			
-			int langResult = 1;
-			if(langNo != null) {
-				for(int i = 0; i < langNo.length; i++) {
-					int languageNo = Integer.parseInt(langNo[i]);
-					langResult = langResult * new InfoDao().nationLang(conn, languageNo, nationNo);
+				
+				int langResult = 1;
+				if(langNo != null) {
+					for(int i = 0; i < langNo.length; i++) {
+						int languageNo = Integer.parseInt(langNo[i]);
+						langResult = langResult * new InfoDao().nationLang(conn, languageNo, nationNo);
+					}
 				}
+				
+				int titleResult = 1;
+				if(title != null) {
+					titleResult = new NationDao().insertTitlePhoto(conn, nationNo, title);
+				}
+				
+				int photoResult = 1;
+				if(file != null) {
+					photoResult = new NationDao().insertPhoto(conn, nationNo, file);
+				}
+				
+				result = nationResult * visaResult * volResult * curResult * langResult * titleResult * photoResult;
 			}
-			
-			int titleResult = 1;
-			if(title != null) {
-				titleResult = new NationDao().insertTitlePhoto(conn, nationNo, title);
-			}
-			
-			int photoResult = 1;
-			if(file != null) {
-				photoResult = new NationDao().insertPhoto(conn, nationNo, file);
-			}
-			
-			result = nationResult * visaResult * volResult * curResult * langResult * titleResult * photoResult;
 		}
 
 		if(result > 0) commit(conn);
@@ -140,8 +134,10 @@ public class NationService {
 	// 국가 추가
 	public int insertNation(Nation nation, int visaNo, String[] volNo, String[] curNo, String[] langNo, AttachmentFile title, AttachmentFile file) {
 		Connection conn = getConnection();
+		
 		int nationNo = nation.getNationNo();
-		int result = 0;
+		
+		int result = 1;
 		
 		int nationResult = new NationDao().insertNation(conn, nation);
 		
@@ -197,15 +193,7 @@ public class NationService {
 		
 		Connection conn = getConnection();
 		
-		int result = 1;
-		
-		if(new InfoDao().countCurrency(conn, nationNo) > 0) new InfoDao().deleteCurrency(conn, nationNo); 
-		if(new InfoDao().countLanguage(conn, nationNo) > 0) new InfoDao().deleteLanguage(conn, nationNo);
-		if(new InfoDao().countVoltage(conn, nationNo) > 0) new InfoDao().deleteVoltage(conn, nationNo);
-		if(new NationDao().selectTitlePhoto(conn, nationNo) != null) new NationDao().deleteTitlePhoto(conn, nationNo);
-		if(new NationDao().selectPhoto(conn, nationNo) != null) new NationDao().deletePhoto(conn, nationNo);
-		new InfoDao().deleteVisa(conn, nationNo);
-		new NationDao().deleteNation(conn, nationNo);
+		int result = new NationDao().deleteNation(conn, nationNo);
 		
 		if(result > 0) commit(conn);
 		close(conn);
